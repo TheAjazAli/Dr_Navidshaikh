bfebef369076f21aa39165cccfff3bcaa8b9e4b3
TheAjazAli <ajazali78890@gmail.com>
2025-05-16 00:11:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+AOS.init({
+                    duration: 1000,  // animation duration in ms
+                    once: true,       // animate only once when scrolling
+                    startEvent:'load' //Wait until Paige is fully loaded
+                });
